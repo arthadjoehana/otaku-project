f5cdb4e966250a26a2f0313c79fbd2232340f517
artha <artha.djoe@gmail.com>
2021-11-12 14:54:11 +0100

    new version

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Menu from './Menu'
+import Search from './Search'
+import LogoBlack from '../../images/logo-black.jpg'
+import LogoWhite from '../../images/logo-white.jpg'
+
+const Header = () => {
+
+    return (
+        <div className="header bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light 
+            bg-light justify-content-between align-middle">
+
+                <Link to="/" className="logo">
+                    <img 
+                        className="navbar-brand text-uppercase p-0 m-0"
+                        src={LogoWhite}
+                        onClick={() => window.scrollTo({top: 0})} 
+                        alt="" />
+                </Link>
+
+                <Search />
+
+                <Menu />
+            </nav>
+        </div>
+    )
+}
+
+export default Header
